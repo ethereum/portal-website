8426f5c9e01d71d211fa89f0f682d627dec6af6d
jmc <jmcook1186@gmail.com>
2023-08-31 17:23:21 +0100

    initial commit for portal website

---
+++ README.md
@@ -0,0 +1,9 @@
+# Portal Network docs
+
+This site is bootstrapped using Nextra (docs theme). To build locally run
+```
+pnpm install
+pnpm dev
+```
+
+from the project root.
---
+++ theme.config.tsx
@@ -0,0 +1,32 @@
+import { DocsThemeConfig, Navbar } from 'nextra-theme-docs'
+import { portalLogo } from './components/logo'
+import NextScript from "next/script";
+
+const config: DocsThemeConfig = {
+  logo: portalLogo,
+  project: {
+    link: 'https://github.com/ethereum/portal-network-specs',
+  },
+  chat: {
+    link: 'https://discord.gg/QcD3wWSp',
+  },
+  docsRepositoryBase: 'https://github.com/ethereum/portal-network-specs',
+  footer: {
+    text: 'Portal Network docs',
+  }
+};
+
+const navbar: Navbar = {
+  height: '8rem'
+
+}
+
+const CustomHead: React.FC = () => (
+  <>
+    <meta name="og:title" content="Portal network" />
+    <meta name="og:description" content="Portal Network: lightweight access to Ethereum" />
+    <meta name="og:image" content="public/logos/EthPortalNetworkLogo.png" />
+  </>
+);
+
+export default { ...config, head: CustomHead };
